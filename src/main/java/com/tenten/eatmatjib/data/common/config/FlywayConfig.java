05e66cbc58c526763bbe0f8b22a9106302eafaf5
GiYeons <65033360+GiYeons@users.noreply.github.com>
2024-08-31 02:33:48 +0900

    [chore] DDL 생성

---
+++ src/main/java/com/tenten/eatmatjib/data/common/config/FlywayConfig.java
@@ -0,0 +1,18 @@
+package com.tenten.eatmatjib.data.common.config;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+    /*  migrate 전에 repair를 먼저 하여 checksum을 재설정해준다
+     형상이 깨져 에러가 발생하는 것을 막아주는 코드  */
+    @Bean
+    public FlywayMigrationStrategy cleanMigrateStrategy() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+    }
+}
