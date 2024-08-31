ec5c90e7c0eff86236f52fd465c3eb68dd02b408
GiYeons <65033360+GiYeons@users.noreply.github.com>
2024-08-31 10:33:06 +0900

    [feat] 초기 데이터 fetch

---
+++ src/main/java/com/tenten/eatmatjib/data/pipeline/Data.java
@@ -0,0 +1,97 @@
+package com.tenten.eatmatjib.data.pipeline;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+public class Data {
+    @JsonProperty("OPNSFTEAMCODE")
+    private String opnsfteamcode;
+    @JsonProperty("MGTNO")
+    private String mgtno;
+    @JsonProperty("APVPERMYMD")
+    private String apvpermymd;
+    @JsonProperty("APVCANCELYMD")
+    private String apvcancelymd;
+    @JsonProperty("TRDSTATEGBN")
+    private String trdstategbn;
+    @JsonProperty("TRDSTATENM")
+    private String trdstatenm;
+    @JsonProperty("DTLSTATEGBN")
+    private String dtlstategbn;
+    @JsonProperty("DTLSTATENM")
+    private String dtlstatenm;
+    @JsonProperty("DCBYMD")
+    private String dcbymd;
+    @JsonProperty("CLGSTDT")
+    private String clgstdt;
+    @JsonProperty("CLGENDDT")
+    private String clgenddt;
+    @JsonProperty("ROPNYMD")
+    private String ropnymd;
+    @JsonProperty("SITETEL")
+    private String sitetel;
+    @JsonProperty("SITEAREA")
+    private String sitearea;
+    @JsonProperty("SITEPOSTNO")
+    private String sitepostno;
+    @JsonProperty("SITEWHLADDR")
+    private String sitewhladdr;
+    @JsonProperty("RDNWHLADDR")
+    private String rdnwhladdr;
+    @JsonProperty("RDNPOSTNO")
+    private String rdnpostno;
+    @JsonProperty("BPLCNM")
+    private String bplcnm;
+    @JsonProperty("LASTMODTS")
+    private String lastmodts;
+    @JsonProperty("UPDATEGBN")
+    private String updategbn;
+    @JsonProperty("UPDATEDT")
+    private String updatedt;
+    @JsonProperty("UPTAENM")
+    private String uptaenm;
+    @JsonProperty("X")
+    private String x;
+    @JsonProperty("Y")
+    private String y;
+    @JsonProperty("SNTUPTAENM")
+    private String sntuptaenm;
+    @JsonProperty("MANEIPCNT")
+    private String maneipcnt;
+    @JsonProperty("WMEIPCNT")
+    private String wmeipcnt;
+    @JsonProperty("TRDPJUBNSENM")
+    private String trdpjubnsenm;
+    @JsonProperty("LVSENM")
+    private String lvsenm;
+    @JsonProperty("WTRSPLYFACILSENM")
+    private String wtrsplyfacilsenm;
+    @JsonProperty("TOTEPNUM")
+    private String totepnum;
+    @JsonProperty("HOFFEPCNT")
+    private String hoffepcnt;
+    @JsonProperty("FCTYOWKEPCNT")
+    private String fctyowkepcnt;
+    @JsonProperty("FCTYSILJOBEPCNT")
+    private String fctysiljobepcnt;
+    @JsonProperty("FCTYPDTJOBEPCNT")
+    private String fctypdtjobepcnt;
+    @JsonProperty("BDNGOWNSENM")
+    private String bdngownsenm;
+    @JsonProperty("ISREAM")
+    private String isream;
+    @JsonProperty("MONAM")
+    private String monam;
+    @JsonProperty("MULTUSNUPSOYN")
+    private String multusnupsoyn;
+    @JsonProperty("FACILTOTSCP")
+    private String faciltotscp;
+    @JsonProperty("JTUPSOASGNNO")
+    private String jtupsoasgnno;
+    @JsonProperty("JTUPSOMAINEDF")
+    private String jtupsomainedf;
+    @JsonProperty("HOMEPAGE")
+    private String homepage;
+}
+
---
+++ src/main/java/com/tenten/eatmatjib/data/pipeline/DataService.java
@@ -0,0 +1,91 @@
+package com.tenten.eatmatjib.data.pipeline;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.net.URLEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+
+@Service
+@RequiredArgsConstructor
+public class DataService {
+
+    @Value("${OPEN_API_KEY}")
+    private String openApiKey;
+
+    private final DataMapper dataMapper;
+    private final RestTemplate restTemplate;
+
+    public void initData() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            // 처음 페이지 요청하여 totalRecords와 첫 페이지 데이터 가져오기
+            String initialUrl = buildUrl(1, 1);
+            ResponseEntity<String> initialResponseEntity = restTemplate.getForEntity(initialUrl, String.class);
+            checkResponse(initialResponseEntity);
+
+            JsonNode initialRootNode = objectMapper.readTree(initialResponseEntity.getBody());
+
+            // "list_total_count" 값을 추출
+            int totalRecords = initialRootNode.path("LOCALDATA_072404").path("list_total_count").asInt();
+
+            // 첫 페이지의 데이터 저장
+            JsonNode initialRowNode = initialRootNode.path("LOCALDATA_072404").path("row");
+            for (JsonNode node : initialRowNode) {
+                Data data = objectMapper.treeToValue(node, Data.class);
+                dataMapper.initData(data);
+            }
+
+            // 나머지 데이터 페이지 단위로 가져오기
+            int pageSize = 1000;
+            for (int idx = 2; idx <= totalRecords; idx += pageSize) {
+                String requestUrl = buildUrl(idx, Math.min(idx + pageSize - 1, totalRecords));
+                ResponseEntity<String> pageResponseEntity = restTemplate.getForEntity(requestUrl, String.class);
+                checkResponse(pageResponseEntity);
+
+                JsonNode pageRootNode = objectMapper.readTree(pageResponseEntity.getBody());
+                JsonNode rowNode = pageRootNode.path("LOCALDATA_072404").path("row");
+
+                for (JsonNode node : rowNode) {
+                    Data data = objectMapper.treeToValue(node, Data.class);
+                    dataMapper.initData(data);
+                }
+            }
+            System.out.println("모든 데이터가 저장되었습니다.");
+        } catch (HttpClientErrorException e) {
+            // HTTP 클라이언트 예외 처리
+            throw new RuntimeException("HTTP Error: " + e.getStatusCode() + " - " + e.getResponseBodyAsString(), e);
+        } catch (Exception e) {
+            // 일반적인 예외 처리
+            throw new RuntimeException("An error occurred while processing the data", e);
+        }
+    }
+
+    private void checkResponse(ResponseEntity<String> responseEntity) {
+        HttpStatusCode statusCode = responseEntity.getStatusCode();
+        if (statusCode.is2xxSuccessful()) {
+            // 200-299 응답 코드 확인
+            return;
+        } else {
+            throw new RuntimeException("Unexpected HTTP status code: " + statusCode);
+        }
+    }
+
+    public String buildUrl(int startIdx, int endIdx) throws Exception {
+        String urlBuilder =
+            "http://openapi.seoul.go.kr:8088" + "/" + URLEncoder.encode(openApiKey, "UTF-8")
+                + "/" + URLEncoder.encode("json", "UTF-8")
+                + "/" + URLEncoder.encode("LOCALDATA_072404", "UTF-8")
+                + "/" + URLEncoder.encode(String.format("%d", startIdx), "UTF-8")
+                + "/" + URLEncoder.encode(String.format("%d", endIdx), "UTF-8");
+
+        return urlBuilder;
+    }
+}
