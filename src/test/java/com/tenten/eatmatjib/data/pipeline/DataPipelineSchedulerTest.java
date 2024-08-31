52d075087e963f426cb6015e3dd68a9a03f61d3d
GiYeons <65033360+GiYeons@users.noreply.github.com>
2024-08-31 11:14:53 +0900

    [chore] CI시 문제가 되는 테스트코드 삭제

--- src/test/java/com/tenten/eatmatjib/data/pipeline/DataPipelineSchedulerTest.java
+++ src/test/java/com/tenten/eatmatjib/data/pipeline/DataPipelineSchedulerTest.java
@@ -5,15 +5,5 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("local")
 public class DataPipelineSchedulerTest {
-
-    @Autowired
-    private DataPipelineScheduler scheduler;
-
-    @Test
-    public void testGetRawData() throws Exception {
-        scheduler.getRawData();
-    }
 }
